adcb88de5f73fc5837931493c5028c8ea6843b62
HumphreyLu6 <zlu@ualberta.ca>
2019-10-17 13:19:56 -0600

    add waypoints

--- navi_bot.py
+++ navi_bot.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # BEGIN ALL
-import rospy, numpy
+import rospy, numpy, actionlib
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
+from move_base_msgs import MoveBaseAction, MoveBaseGoal
 import time
 import smach
 import smach_ros
@@ -31,14 +32,43 @@ class Navigate(smach.State):
     def __init__(self):
         smach.State.__init__(self, 
                              outcomes=['navigating', 'arrived', 'end'])
+
+        self.client = actionlib.SimpleActionClient('move_base', MoveBaseAction) 
+        self.client.wait_for_server()
     
     def execute(self, userdata):
-        global g_start, g_targets
+        global g_start
         if rospy.is_shutdown:
             return 'end'
         else:
-            return 'end'
+            waypoints = self.get_waypoints()
+            for pose in waypoints:   
+                goal = goal_pose(pose)
+                client.send_goal(goal)
+                client.wait_for_result()
+            return 'arrived'
+    
+    def goal_pose(self, pose): 
+        goal_pose = MoveBaseGoal()
 
+        goal_pose.target_pose.header.frame_id = 'map'
+
+        goal_pose.target_pose.pose.position.x = pose[0][0]
+        goal_pose.target_pose.pose.position.y = pose[0][1]
+        goal_pose.target_pose.pose.position.z = pose[0][2]
+        
+        goal_pose.target_pose.pose.orientation.x = pose[1][0]
+        goal_pose.target_pose.pose.orientation.y = pose[1][1]
+        goal_pose.target_pose.pose.orientation.z = pose[1][2]
+        goal_pose.target_pose.pose.orientation.w = pose[1][3]
+
+        return goal_pose
+    
+    def get_waypoints(self):
+        global g_targets
+        waypoints = [(2.1, 2.2, 0.0), (0.0, 0.0, 0.0, 1.0)]
+        return waypoints
+            
 def joy_callback(msg):
     global g_targets, g_start
     if len(g_targets) <= 4:
